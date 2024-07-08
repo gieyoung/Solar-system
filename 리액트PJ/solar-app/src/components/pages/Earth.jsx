c2a0e854a04c6a765e9e0bbeabd945edb416ce3f
KO <j316ko@naver.com>
2024-07-08 16:29:51 +0900

    124124

---
+++ 리액트PJ/solar-app/src/components/pages/Earth.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Esection1 from '../modules/Esection1';
+import Esection2 from '../modules/Esection2';
+import Esection3 from '../modules/Esection3';
+import Esection4 from '../modules/Esection4';
+
+function Earth(props) {
+    return (
+        <>
+            <Esection1/>
+            <Esection2/>
+            <Esection3/>
+            <Esection4/>
+        </>
+    );
+}
+
+export default Earth;
