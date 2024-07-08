9a4b97a444beb13beedd4006c1addbd0db152c80
gieyoung <poohlee23@naver.com>
2024-07-08 16:22:37 +0900

    f

--- 리액트PJ/solar-app/src/components/layout/TopArea.jsx
+++ 리액트PJ/solar-app/src/components/layout/TopArea.jsx
@@ -1,13 +1,68 @@
 // 상단영역 컴포넌트 ///
+import React from "react";
 
-
-import React from 'react';
+// 상단영역 CSS 불러오기
+import "../../css/top_area.scss";
 
 function TopArea(props) {
   return (
-    <div>
-      탑영역
-    </div>
+    <>
+      <header className="header-area inbox">
+        {/* <!-- 로고 --> */}
+        <h1 className="logo">
+          <a href="./" alt="나사 로고">
+            <span className="ir">로고</span>
+            <img src="./images/common/logo.png" alt="로고이미지" />
+          </a>
+        </h1>
+
+        {/* <!-- 메뉴 --> */}
+        <nav id="gnb"></nav>
+
+        {/* <!-- 기타 --> */}
+        <div className="etc">
+          {/* <!-- 검색버튼 --> */}
+          <div className="search">
+            <input type="text" id="input-box" className="input-box" />
+            <label for="input-box" className="fa-solid fa-magnifying-glass icon"></label>
+          </div>
+
+          {/* <!-- 모바일 햄버거 버튼 --> */}
+          <div className="mobile-btn">
+            <span className="ir">모바일버튼</span>
+            <i className="fa-solid fa-bars icon2">
+              {" "}
+              <span className="ir">햄버거 아이콘</span>
+            </i>
+            <i className="fa-solid fa-xmark icon3">
+              {" "}
+              <span className="ir">닫기 아이콘</span>
+            </i>
+          </div>
+
+          {/* <!-- 모바일 사이트맵 --> */}
+          <div className="m-sitemap">
+            <ul>
+              <li>
+                <a href="./sun.html">sun</a>
+              </li>
+              <li>
+                <a href="./earth.html">earth</a>
+              </li>
+              <li>
+                <a href="./moon.html">moon</a>
+              </li>
+              <li>
+                <a href="./saturn.html">saturn</a>
+              </li>
+              <li>
+                <a href="./jupiter.html">jupiter</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
 
