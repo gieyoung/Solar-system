491cff51cddd8509a4cf6d237b69bab506f5b4cc
KO <j316ko@naver.com>
2024-07-08 17:03:03 +0900

    124

--- 리액트PJ/solar-app/src/components/pages/Earth.jsx
+++ 리액트PJ/solar-app/src/components/pages/Earth.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Esection1 from '../modules/Esection1';
 import Esection2 from '../modules/Esection2';
 import Esection3 from '../modules/Esection3';
 import Esection4 from '../modules/Esection4';
+import '../../css/earth.scss';
 
-function Earth(props) {
+function Earth() {
+    useEffect(() => {
+        const handleScroll = (e) => {
+            e.preventDefault();
+            const { deltaY } = e;
+            const sectionHeight = window.innerHeight;
+
+            // Determine the new scroll position
+            let newScroll = window.scrollY + (deltaY > 0 ? sectionHeight : -sectionHeight);
+
+            // Ensure the new scroll position is within bounds
+            newScroll = Math.max(0, Math.min(newScroll, sectionHeight * 3));
+
+            window.scrollTo({
+                top: newScroll,
+                behavior: 'smooth',
+            });
+        };
+
+        window.addEventListener('wheel', handleScroll, { passive: false });
+
+        return () => {
+            window.removeEventListener('wheel', handleScroll);
+        };
+    }, []);
+
     return (
-        <>
-            <Esection1/>
-            <Esection2/>
-            <Esection3/>
-            <Esection4/>
-        </>
+        <div className="earth-section">
+            <Esection1 />
+            <Esection2 />
+            <Esection3 />
+            <Esection4 />
+        </div>
     );
 }
 
