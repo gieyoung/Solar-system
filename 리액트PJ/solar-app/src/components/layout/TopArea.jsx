23643f7f5abf1b5a3c1d1db9a901a64a10f6897a
gieyoung <poohlee23@naver.com>
2024-07-09 17:45:52 +0900

    d

--- 리액트PJ/solar-app/src/components/layout/TopArea.jsx
+++ 리액트PJ/solar-app/src/components/layout/TopArea.jsx
@@ -3,6 +3,9 @@ import React, { useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
+// 데이터 불러오기
+import { menu } from "../data/gnb";
+
 //js불러오기
 import topFn from "../func/top_area.js";
 
@@ -17,9 +20,7 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-
 function TopArea({ loginSts, logoutFn }) {
-
   useEffect(() => {
     //상단공통함수
     topFn();
@@ -42,60 +43,50 @@ function TopArea({ loginSts, logoutFn }) {
           {/* <!-- 메뉴 --> */}
           <nav id="gnb">
             <ul>
-              <li></li>
-              <li>
-                <a href="./earth.html">earth</a>
-              </li>
-
-              <li>
-                <a href="./moon.html">moon</a>
-              </li>
-
-              <li>
-                <a href="./saturn.html">saturn</a>
-              </li>
-
-              <li>
-                <a href="./jupiter.html">jupiter</a>
-              </li>
+              {menu.map((v, i) => (
+                <li key={i} className="gnb-depth1">
+                  <Link to={v.link}>
+                    <span>{v.menu}</span>
+                  </Link>
+                </li>
+              ))}
             </ul>
           </nav>
 
           {/* <!-- 기타 --> */}
           <div className="etc">
             <ul>
-
-                  {
-                    // 로그인 상태가 null일때 나옴
-                    loginSts === null && (
-                      <>
-                        <li>
-                          <Link to="/member">JOIN US</Link>
-                        </li>
-                        <li>
-                          <Link to="/login">LOGIN</Link>
-                        </li>
-                      </>
-                    )
-                  }
-                  {
-                    // 로그인 상태가 null이 아니면
-                    loginSts !== null && (
-                      <>
-                        <li>
-                          <a
-                            href="#"
-                            onClick={(e) => {
-                              e.preventDefault();
-                              logoutFn();
-                            }}
-                          >
-                            LOGOUT
-                          </a>
-                        </li>
-                      </>
-                    )
-                  }
+              {
+                // 로그인 상태가 null일때 나옴
+                loginSts === null && (
+                  <>
+                    <li>
+                      <Link to="/member">JOIN US</Link>
+                    </li>
+                    <li>
+                      <Link to="/login">LOGIN</Link>
+                    </li>
+                  </>
+                )
+              }
+              {
+                // 로그인 상태가 null이 아니면
+                loginSts !== null && (
+                  <>
+                    <li>
+                      <a
+                        href="#"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          logoutFn();
+                        }}
+                      >
+                        LOGOUT
+                      </a>
+                    </li>
+                  </>
+                )
+              }
             </ul>
             {/* <!-- 검색버튼 --> */}
             <div className="search">
@@ -105,7 +96,6 @@ function TopArea({ loginSts, logoutFn }) {
                 {/* 폰트어썸 사용 */}
                 <FontAwesomeIcon icon={faMagnifyingGlass} />
               </div>
-
             </div>
 
             {/* <!-- 모바일 햄버거 버튼 --> */}
@@ -114,7 +104,6 @@ function TopArea({ loginSts, logoutFn }) {
                 {/* 폰트어썸 사용 */}
                 <FontAwesomeIcon icon={faBars} />
                 <span className="ir">모바일 버튼</span>
-        
               </div>
               <div className="icon3">
                 {/* 폰트어썸 사용 */}
