175315d87f738db82cb0c65fbc2cef400c48e209
ji0 <buti12@naver.com>
2024-07-09 15:51:37 +0900

    g

---
+++ 리액트PJ/solar-app/src/components/js/jupiter_moons_data.js
@@ -0,0 +1,23 @@
+export default[
+    {
+       idx:"1",
+       name:"Ganymede",
+       txt:"Ganymede is Jupiter's largest moon, and the largest moon in our solar system. It's even bigger than Mercury, and Pluto.",
+    },
+    {
+       idx:"2",
+       name:"Callisto",
+       txt:"Callisto is Jupiter’s second largest moon and the third largest moon in our solar system.",
+    },
+    {
+       idx:"3",
+       name:"Io",
+       txt:"Io is Jupiter's third largest moon, and the most volcanically active world in our solar system.",
+    },
+    {
+       idx:"4",
+       name:"Ganymede",
+       addr:"https://science.nasa.gov/jupiter/moons/ganymede/",
+       txt:"Ganymede is Jupiter's largest moon, and the largest moon in our solar system. It's even bigger than Mercury, and Pluto.",
+    },
+]
