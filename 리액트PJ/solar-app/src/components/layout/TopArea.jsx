ae4f4694b5768c7867702226e6809a5cc041d225
gieyoung <poohlee23@naver.com>
2024-07-10 14:32:07 +0900

    f

--- 리액트PJ/solar-app/src/components/layout/TopArea.jsx
+++ 리액트PJ/solar-app/src/components/layout/TopArea.jsx
@@ -45,7 +45,7 @@ function TopArea({ loginSts, logoutFn }) {
           <nav id="gnb">
             <ul>
               {menu.map((v, i) => (
-                <li key={i} className="gnb-depth1">
+                <li key={i}>
                   <Link to={v.link}>
                     <span>{v.menu}</span>
                   </Link>
